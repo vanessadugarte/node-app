97dfc41a971ee4963b5c8c83ff75f98fe7d906b5
Vanessa V Dugarte <dugartevanessa@gmail.com>
2024-07-04 12:58:22 -0400

    add cartRouter

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require ('express');
 const app = express();
 const productsRouter = require ("./routes/products");
+const cartsRouter = require ("./routes/cart");
 
 app.use(express.json());
 app.use('/api/products', productsRouter)
+app.use('/api/carts', cartsRouter)
+
 
 const PORT = 8080;
 app.listen(PORT, () => {
