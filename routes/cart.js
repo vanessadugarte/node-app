ea57dc7f1a07280b0a19b496f05758eb2f092ba3
Vanessa V Dugarte <dugartevanessa@gmail.com>
2024-07-07 22:35:38 -0400

    add product to cart endpoint

--- routes/cart.js
+++ routes/cart.js
@@ -5,7 +5,9 @@ const fs = require ('fs');
 const { v4: uuidv4 } = require('uuid');
 
 const cartFilePath = path.join(__dirname, '../data/cart.json');
+const productFilePath = path.join(__dirname, '../data/product.json');
 const readCarts= () => JSON.parse(fs.readFileSync(cartFilePath, 'utf8')) ;
+const readProducts= () => JSON.parse(fs.readFileSync(productFilePath, 'utf8')) ;
 const writeCarts = (data) => fs.writeFileSync(cartFilePath, JSON.stringify(data, null, 2));
 
 router.get( '/:cid', (req,res) => {
@@ -24,13 +26,37 @@ router.post( '/', (req,res) => {
         const carts = readCarts();
         const newCart = {
             id: uuidv4(),
-            product: []
+            products: []
         }
         carts.push(newCart);
         writeCarts(carts);
         res.status(201).json(newCart);
 
 
+});
+
+router.post( '/:cid/product/:pid', (req,res) => {
+    const carts = readCarts();
+    const products = readProducts();
+    const cart = carts.find((el)=>{
+        return el.id === req.params.cid
+    })
+    if(!cart){
+       return res.status(404).send("Carro no encontrado");
+    }
+    const product = cart.products.findIndex((el)=>{
+        return el.product === req.params.pid
+    })
+    if(product !== -1) {
+       cart.products[product].quantity += 1;
+    } else {
+        cart.products.push({
+            product: req.params.pid,
+            quantity:1
+        })
+    }
+    writeCarts(carts)
+    res.status(201).json(cart)
 });
 
 module.exports = router;
